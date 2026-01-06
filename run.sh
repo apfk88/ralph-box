d97378c79f4115b2e127348528ebbaa16b8797d0
apfk88 <apfk88@gmail.com>
2026-01-06 13:29:53 -0800

    Run container as host user UID/GID for volume permissions

--- run.sh
+++ run.sh
@@ -7,6 +7,10 @@ if [[ -z "$REPO_URL" ]]; then
   read -p "Repo URL (or press enter to skip): " REPO_URL
 fi
 
+# Export UID/GID so container runs as current user
+export UID=$(id -u)
+export GID=$(id -g)
+
 if [[ -n "$REPO_URL" ]]; then
   docker-compose run --rm -e REPO_URL="$REPO_URL" ralph
 else
